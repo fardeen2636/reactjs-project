d21749283942db3766850031698a6ce4ba84b33b
fardeen <fardeenhussain36@gmail.com>
2023-12-29 19:09:20 +0530

    Minor Updation

--- components/landingPage/becomeAnIstructor.js
+++ components/landingPage/becomeAnIstructor.js
@@ -38,8 +38,8 @@ export default function BecomeAnInstructor() {
                     <div className="col-lg-7">
                       <h3 className="text-white">Become an Instructor</h3>
                       <p className="text-white mb-3 mb-lg-0">
-                      It is very important for the customer to pay attention to the adipiscing process.
-                      He is nothing more than an accuser of finding pleasures!
+                        It is very important for the customer to pay attention to the adipiscing process.
+                        He is nothing more than an accuser of finding pleasures!
                       </p>
                     </div>
                     <div className="col-lg-5 text-lg-end">
